4cdaa6e2df063bb56809ace5f34efa280b56dbdf
Алексей <lexus1083@gmail.com>
2024-11-27 22:52:50 +0300

    ad file

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,28 +1,48 @@
 import telebot
 import webbrowser
+from telebot import types
 
 bot = telebot.TeleBot('7972137837:AAHjsNoxlogcQYXeEECIBuD31u-oQN8D3Do')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton('Перейти на сайт')
+    markup.row(btn1)
+    btn2 = types.KeyboardButton('Удалить фото')
+    btn3 = types.KeyboardButton('Изменить текст!')
+    markup.row(btn2, btn3)
+    file = open('./photo.jpg', 'rb')
+    bot.send_photo(message.chat.id, file, reply_markup=markup)
+   # bot.send_message(message.chat.id, 'Привет', reply_markup=markup)
+    bot.register_next_step_handler(message, on_click)
+
+def on_click(message):
+    if message.text == 'Перейти на сайт':
+        bot.send_message(message.chat.id, 'Вебсайт открыт')
+    elif message.text == 'Удалить фото':
+        bot.send_message(message.chat.id, 'Фото удалено')
+    elif message.text == 'Изменить текст!':
+        bot.send_message(message.chat.id, 'Текст изменен')
+
+
 
 @bot.message_handler(commands=['site', 'website'])
 def site(message):
     webbrowser.open('https://itproger.com')
 
 
-
-@bot.message_handler()
-def info(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}!')
-    elif message.text.lower() == 'id':
-        bot.reply_to(message, f'id: {message.from_user.id}')
-
 @bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}!')
 
+
 @bot.message_handler(commands=['help'])
 def main(message):
-    bot.send_message(message.chat.id, '<b><em>Информация</em></b>', parse_mode='html') # форматирование текста в режиме html
+    bot.send_message(message.chat.id, '<b><em>Информация</em></b>',
+                     parse_mode='html')  # форматирование текста в режиме html
+
 
 @bot.message_handler()
 def info(message):
@@ -30,6 +50,25 @@ def info(message):
         bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}!')
     elif message.text.lower() == 'id':
         bot.reply_to(message, f'id: {message.from_user.id}')
+
+
+@bot.message_handler(content_types=['photo'])
+def get_photo(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('Перейти на сайт', url='https://google.com')
+    markup.row(btn1)
+    btn2 = types.InlineKeyboardButton('Удалить фото', callback_data='delete')
+    btn3 = types.InlineKeyboardButton('Изменить текст!', callback_data='edit')
+    markup.row(btn2, btn3)
+    bot.reply_to(message, 'Какое красивое фото!', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == 'delete':
+        bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
+    elif callback.data == 'edit':
+        bot.edit_message_text('Edit text', callback.message.chat.id, callback.message.message_id)
 
 
 bot.polling(non_stop=True)
